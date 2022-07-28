36704102d83ee0ec0ab5ab621373c973ac5ebcb9
Uttam Dev Sapkota <110143141+Uttammsapk@users.noreply.github.com>
2022-07-27 22:24:29 -0400

    Add files via upload

---
+++ Total_sale.py
@@ -0,0 +1,7 @@
+# Find the total profit of the company
+totalsale = float(input("What is the total sale of the company this year: "))
+
+#The profit i 23 %
+
+profit =(totalsale*23/100)
+print("The total profit of the company is", profit)
