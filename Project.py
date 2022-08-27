c73827c9ff8a4d01d63d8cc1d305a6f95efe2fd6
Uttam Dev Sapkota <110143141+Uttammsapk@users.noreply.github.com>
2022-08-27 11:40:53 -0400

    Add files via upload

---
+++ Project.py
@@ -0,0 +1,40 @@
+# Let's make a matlib
+'''adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+funny = input("Funny_person: ")
+
+madlib = "I am in such a love with {adj} that I can't  explain why I am {verb1} and why will I be a {verb2}.\
+  But I really want to be great like a {funny}".format(adj=adj, verb1 = verb1, verb2=verb2,funny=funny)
+
+print(madlib)
+
+a = input("Who is your favourite person: ")
+b = int(input("Enter any number: "))
+c = input("Who is your hatest person: ")
+madlib = "Omg I really love {a} {b} times more than {c}.".format(a=a,b=b,c=c)
+print(madlib)
+
+
+import time
+username = "Aashik"
+password = "Jinkara12"
+
+username_ = input("Enter username : ")
+password_ = input("Enter your username: ")
+
+if username == username_ and password == password_:
+  print("Checking information.....")
+  time.sleep(5)
+  print("User Found")
+  print("Loading information...")
+  time.sleep(6)
+  print("Access granted...")
+  print("taking useer to the secret information ")
+elif username == username and password != password_:
+  print("Incorrect Password")
+elif username != username_ and password == password_:
+  print("Incorrect Username")
+else : 
+  print("Incorrect Username and Password")'''
+
---
+++ multiplication_table.py
@@ -0,0 +1,9 @@
+print("Let's get the multiplication table")
+a = 0
+b = int(input("Enter number: "))
+c = b*10
+
+
+while a<=c:
+    print(a)
+    a = a+b
