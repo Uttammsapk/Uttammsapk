36521a9b2536f6048a2a0ee9374eefd6ada25dcd
Uttam Dev Sapkota <110143141+Uttammsapk@users.noreply.github.com>
2022-10-15 01:32:35 -0400

    Add files via upload

---
+++ for.py
@@ -0,0 +1,16 @@
+class Bank():
+  def __init__(self,name,pin,deposit,withdraw):
+    self.name = name 
+    self.pin = pin
+    self.deposit = deposit
+    self.withdraw = withdraw
+  
+  def depo(self,deposit):
+    a = "Enter the amount you want to deposit: "
+    deposit ==a
+
+  def depo(self,withdraw):
+    b = "Enter the amount you want to deposit: "
+    withdraw ==b
+
+print(Bank("aashik",1234, 20, 30))
